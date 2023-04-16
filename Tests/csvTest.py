d62f3de916b4af5a824ee7f1aa8dfc4c7a114a49
Maaitrayo Das <dasmaaitrayo@gmail.com>
2023-04-16 11:19:41 +0530

    result generation

---
+++ Tests/csvTest.py
@@ -0,0 +1,19 @@
+import csv
+
+header = ['name', 'area', 'country_code2', 'country_code3']
+data = [
+    ['Albania', 28748, 'AL', 'ALB'],
+    ['Algeria', 2381741, 'DZ', 'DZA'],
+    ['American Samoa', 199, 'AS', 'ASM'],
+    ['Andorra', 468, 'AD', 'AND'],
+    ['Angola', 1246700, 'AO', 'AGO']
+]
+
+with open('countries.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+
+    # write multiple rows
+    writer.writerows(data)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import supervision as sv
+import os
+import csv
 
 from ultralytics import YOLO
 
@@ -13,25 +15,40 @@ frame_hyt = 480
 
 cap = cv2.VideoCapture(0)
 
-def main():
-    if not cap.isOpened():
-        print("Cannot open file")
-        exit()
+def imageLoader(folder_path):
+    items = os.listdir(folder_path)
+    print(f"[!] Found {len(items)} images [!]")
+
+    images_path_list = []
+    for image in items:
+        item_path = os.path.join(folder_path, image)
+        images_path_list.append(item_path)
+    # print(item_path)
+    return(images_path_list, items)
+
+def saveResultCSV(result):
+    with open('result.csv','w') as f1:
+        writer=csv.writer(f1, delimiter=',')#lineterminator='\n',
+        for i in range(len(result)):
+            row = result[i]
+            writer.writerow(row)
+
+def processImages(image_path_list, image_name_list):
+    # if not cap.isOpened():
+    #     print("Cannot open file")
+    #     exit()
 
     box_annotator = sv.BoxAnnotator(
         thickness=2,
         text_thickness=1,
         text_scale=1
     )
-    count = 0
-    while True:
-        # ret, frame = cap.read()
-        # frame = cv2.imread("D:\ANTEYE INTERNSHIP\HELMET DETECTION\DATASETS\DATASET LARGE\images\hard_hat_workers0.png") 
-        frame = cv2.imread("D:\ANTEYE INTERNSHIP\HELMET DETECTION\RESOURCES\img2.jpeg")
+    # count = 0
+    # while True:
+    csv_result_msg_final = []
 
-        # if not ret:
-        #     print("Can't receive frame (stream end?). Exiting ...")
-        #     break
+    for i in range(len(image_path_list)):
+        frame = cv2.imread(image_path_list[i])
 
         image = cv2.resize(frame, (frame_wid, frame_hyt))
         
@@ -49,15 +66,30 @@ def main():
         )
         if "head" in labels:
             print("head found")
-            image_name = f"{count}.png"
-            image_loc = "records/"+image_name
+            image_name = f"{image_name_list[i]}"
+            # image_loc = "records/"+image_name
+            image_loc = os.path.join("records/",image_name)
             cv2.imwrite(image_loc, image)
+
+            csv_result_msg_row = []
+            img_loc = image_path_list[i]
+            message = "No Helmet"
+            # csv_result_msg_row.append(img_loc)
+            # csv_result_msg_row.append(message)
+            csv_result_msg_final.append([img_loc, message])
+            # csv_result_msg_row.clear()
 
         cv2.imshow("Helmet Detection", image)
 
-        if (cv2.waitKey(1) == 27):
+        if (cv2.waitKey(0) == 27):
             break
 
+    return csv_result_msg_final
+
 if __name__ == "__main__":
-    main()
+    folder_path = input("Enter the folder path : ")
+    # main()
+    image_path_list, image_name_list = imageLoader(folder_path)
+    result = processImages(image_path_list, image_name_list)
+    saveResultCSV(result)
 
